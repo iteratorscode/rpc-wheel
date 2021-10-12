63e13b319054de2880aecf1e3d9fe4dea63e5326
iterators <zhanghui15@baijiahulian.com>
2021-10-12 10:08:16 +0800

    feat: 添加定义

---
+++ common/src/main/java/service/annotation/RpcReference.java
@@ -0,0 +1,12 @@
+package service.annotation;
+
+/**
+ * Rpc服务引用
+ *
+ * @author iterators
+ * @since 2021/10/12 10:07 上午
+ */
+public @interface RpcReference {
+
+    String serviceName() default "";
+}
